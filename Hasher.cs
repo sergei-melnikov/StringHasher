15d972c74ee0821cda1c4cb6ba1207bf7ef7f21a
sergei-melnikov <melnikhov@gmail.com>
2020-06-07 22:00:26 +0300

    Initial commit

---
+++ Hasher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace LinkHasher
+{
+    public class Hasher 
+    {
+        public string GetStringToHash(string inputData)
+        {
+            byte[] byteArrayPhoneNumber = Encoding.UTF8.GetBytes(inputData);
+
+            SHA256 sha256Hash = SHA256.Create();
+            HashAlgorithm hashAlgorithm = sha256Hash;
+            byte[] phoneHashArray = hashAlgorithm.ComputeHash(byteArrayPhoneNumber);
+            
+            var sBuilder = new StringBuilder();
+            for (int i = 0; i < phoneHashArray.Length; i++)
+            {
+                sBuilder.Append(phoneHashArray[i].ToString("x2"));
+            }
+
+            return sBuilder.ToString();
+        }
+
+        public string GetFirstThreeBytes(string inputData)
+        {
+            return inputData.Remove(6);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace LinkHasher
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            string phoneNumber = "79054991616";
+            Hasher phoneHash = new Hasher();
+            string result = phoneHash.GetStringToHash(phoneNumber);
+
+            Console.WriteLine(result);
+
+            string cropResult = phoneHash.GetFirstThreeBytes(result);
+            Console.WriteLine(cropResult);
+        }
+
+
+    }
+}
